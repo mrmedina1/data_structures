c4233ce9c3a974d6795bfb74d902c058780a9561
mrmedina1 <mrmedina@ucdavis.edu>
2019-10-15 16:13:44 -0700

    added BST.java

---
+++ BST.java
@@ -0,0 +1,33 @@
+package data_structures;
+
+public class BST {
+	private static class Node {
+		private int data;
+		private Node leftChild;
+		private Node rightChild;
+		
+		private Node() {
+			data = -1;
+			leftChild = null;
+			rightChild = null;
+		}
+
+		private Node(int data) {
+			this.data = data;
+			leftChild = null;
+			rightChild = null;
+		}
+	}
+	
+	private Node root;
+	private int counter;
+	
+	public BST() {
+		root = null;
+	}
+	
+	public boolean isEmpty() {
+		return (root == null);
+	}
+	
+}
