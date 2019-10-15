9d7519decfbea96dcbea5af2f6ffeec3885ad121
mrmedina1 <mrmedina@ucdavis.edu>
2019-10-15 15:40:44 -0700

    updated BSTGeneric.java

--- BSTGeneric.java
+++ BSTGeneric.java
@@ -1,7 +1,39 @@
 package data_structures;
 
 public class BSTGeneric<T extends Comparable<T>> {
-	private class Node<T> {
+	private static class Node<T> {
+		private T data;
+		private Node<T> leftChild;
+		private Node<T> rightChild;
 		
+		private Node() {
+			data = null;
+			leftChild = null;
+			rightChild = null;
+		}
+
+		private Node(T data) {
+			this.data = data;
+			leftChild = null;
+			rightChild = null;
+		}
+	}
+	
+	private Node<T> root;
+	private int counter;
+	
+	public BSTGeneric() {
+		root = null;
+	}
+	
+	public boolean isEmpty() {
+		return (root == null);
+	}
+	
+	public void insert(T data){
+		/*Node<T> newNode = new Node<T>(data);
+		if (root == null)
+			root = newNode;
+		if (root != null)*/
 	}
 }
